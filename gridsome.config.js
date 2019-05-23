106d3897b6af8a33a3ef0823296d979891e84154
Gift <laurandidi21@gmailcom>
2019-05-23 13:07:02 +0100

    Add filesystem plugin

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,19 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'Gridsome Blog',
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'blog/**/*.md',
+        typeName: 'BlogPost',
+        route: '/blog/:slug'
+      }
+    }
+  ]
 }
