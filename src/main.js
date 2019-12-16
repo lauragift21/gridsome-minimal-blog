9c4887d24d24a249c24cf654543a2245ad6f5b70
lauragift21 <laurandidi21@gmail.com>
2019-12-16 07:06:51 +0100

    update remark pluginn

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import 'prismjs/themes/prism.css'
+
 
 export default function (Vue, { router, head, isClient }) {
   head.link.push({
